4b0c6e719d33c312f3b48b8ef3fb397d96bed9bf
mizkuzy <Allright22>
2019-02-26 08:28:33 +0300

    Add App component to show list items and remove items

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
+const
+  list = [{
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectId: 0,
+  }, {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectId: 1,
+  },];
+
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state =
+      {
+        list
+      }
+    ;
+
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onDismiss(id) {
+    this.setState(
+      {
+        list: this.state.list.filter(item => item.objectId !== id)
+      }
+    )
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.list.map(item => (
+          <div key={item.objectId}>
+            <span> <a href={item.url}>{item.title}</a> </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            <span>
+              <button onClick={() => this.onDismiss(item.objectId)} type="button">
+                Dismiss
+              </button>
+            </span>
+          </div>
+        ))}
+
       </div>
     );
   }
