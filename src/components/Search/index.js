402862e0a386798eee2884bccb8a40a16f42882c
mizkuzy <Allright22>
2019-03-24 13:01:26 +0300

    Focus search element

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
-const Search = ({value, onChange, onSubmit, children}) =>
-  <form onSubmit={onSubmit}>
-    <input
-      type="text"
-      value={value}
-      onChange={onChange}
-    />
-    <button type="submit">
-      {children}
-    </button>
-  </form>;
+class Search extends Component {
+  componentDidMount() {
+    this.input.focus();
+  }
+
+  render() {
+    const {value, onChange, onSubmit, children} = this.props
+    return (
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={value}
+          onChange={onChange}
+          ref={(node) => { this.input = node; }} // get access to node element in stateless component
+        />
+        <button type="submit">
+          {children}
+        </button>
+      </form>
+    );
+  }
+}
 
 Search.propTypes = {
   value: PropTypes.string,
